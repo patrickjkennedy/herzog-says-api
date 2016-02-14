9e318962af5210c63e3f01481a4d203b910f5e3d
pkennedy89 <patrickken@gmail.com>
2016-02-13 20:20:31 -0800

    First commit & working

---
+++ README.md
@@ -0,0 +1,4 @@
+Herzog Says API
+
+The custom API for generating quotes for 'Herzog Says', an upcoming Slack integration.
+
---
+++ api.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restful import Resource, Api
+from flask.ext.mysql import MySQL
+from random import randint
+import json
+
+mysql = MySQL()
+
+#Initialize Flask app
+app = Flask(__name__)
+
+#MySQL Config
+app.config['MYSQL_DATABASE_USER'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = ''
+app.config['MYSQL_DATABASE_HOST'] = ''
+
+mysql.init_app(app)
+
+api = Api(app)
+
+class Quote(Resource):
+    def get(self):
+        try:
+
+        #Connect to database
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+        #Count rows
+            query = "SELECT COUNT(*) FROM quotes"
+            cursor.execute(query)
+            numRows = cursor.fetchall()
+
+        #Pick random row
+            i = randint(0,numRows[0][0])
+
+        #Query db for quote
+            query = "SELECT * FROM quotes WHERE id=" + str(i)
+            cursor.execute(query)
+            response = cursor.fetchall()
+
+            return {'data': response}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+
+api.add_resource(Quote,'/quote')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
